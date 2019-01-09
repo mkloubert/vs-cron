1a1b3bdd428c95d35e4dfa7b60092d29ef3af933
Marcel Kloubert <marcel.kloubert@gmx.net>
2019-01-09 22:02:27 +0100

    deprecated message

--- src/controller.ts
+++ src/controller.ts
@@ -272,7 +272,9 @@ export class Controller implements vscode.Disposable {
         this._config = cfg;
         this._htmlDocs = [];
 
-        this.showNewVersionPopup();
+        this.showDeprecatedMessage().then(() => {
+        }).catch(() => {
+        });
 
         this.reloadJobs();
     }
@@ -410,74 +412,49 @@ export class Controller implements vscode.Disposable {
         });
     }
 
-    /**
-     * Shows the popup for a new version.
-     */
-    protected showNewVersionPopup() {
-        let me = this;
+    private async showDeprecatedMessage() {
+        const KEY_SHOW_DEPRECATED_MESSAGE = 'vscjShowDeprecatedMessage';
 
-        let pkg = me.packageFile;
-        if (!pkg) {
-            return;
-        }
-
-        let currentVersion = pkg.version;
-        if (!currentVersion) {
-            return;
-        }
-
-        const KEY_LAST_KNOWN_VERSION = 'vscjLastKnownVersion';
-
-        // update last known version
-        let updateCurrentVersion = false;
         try {
-            let lastKnownVersion: any = this._CONTEXT.globalState.get(KEY_LAST_KNOWN_VERSION, false);
-            if (lastKnownVersion != currentVersion) {
-                if (!cj_helpers.toBooleanSafe(this.config.disableNewVersionPopups)) {
-                    // tell the user that it runs on a new version
-                    updateCurrentVersion = true;
+            this._CONTEXT
+                .globalState
+                .update('vscjLastKnownVersion', undefined);
+        } catch { }
 
-                    // [BUTTON] show change log
-                    let changeLogBtn: cj_contracts.PopupButton = {
-                        action: () => {
-                            cj_helpers.open('https://github.com/mkloubert/vs-cron/blob/master/CHANGELOG.md').then(() => {
-                            }, (err) => {
-                                me.log(`[ERROR] Controller.showNewVersionPopup(4): ${cj_helpers.toStringSafe(err)}`);
-                            });
-                        },
-                        title: 'Show CHANGELOG',
-                    };
+        let newShowMessageValue: boolean;
+        try {
+            const SHOW_MESSAGE = cj_helpers.toBooleanSafe(
+                this._CONTEXT.globalState.get(KEY_SHOW_DEPRECATED_MESSAGE, true),
+                true
+            );
 
-                    vscode.window
-                          .showInformationMessage(`You have installed the new version ${currentVersion} of 'vs-cron'!`,
-                                                  changeLogBtn)
-                          .then((item) => {
-                                    if (!item || !item.action) {
-                                        return;
-                                    }
+            newShowMessageValue = SHOW_MESSAGE;
 
-                                    try {
-                                        item.action();
-                                    }
-                                    catch (e) { 
-                                        me.log(`[ERROR] Controller.showNewVersionPopup(3): ${cj_helpers.toStringSafe(e)}`);
-                                    }
-                                });
+            if (SHOW_MESSAGE) {
+                const BTN = await vscode.window.showWarningMessage(
+                    "[vs-cron] The extension has been DEPRECATED. You can try out 'vscode-powertools' for the future ...",
+                    "Open 'vscode-powertools'", "Maybe next time", "Do not show again"
+                );
+
+                if ('Maybe next time' === BTN) {
+                    newShowMessageValue = true;
+                } else {
+                    newShowMessageValue = false;
+                }
+
+                if ("Open 'vscode-powertools'" === BTN) {
+                    await cj_helpers.open('https://marketplace.visualstudio.com/items?itemName=ego-digital.vscode-powertools');
                 }
             }
-        }
-        catch (e) { 
-            me.log(`[ERROR] Controller.showNewVersionPopup(2): ${cj_helpers.toStringSafe(e)}`);
-        }
-
-        if (updateCurrentVersion) {
-            // update last known version
+        } catch (e) {
+            this.log(`[ERROR] Controller.showDeprecatedMessage(1): ${cj_helpers.toStringSafe(e)}`);
+        } finally {
             try {
-                this._CONTEXT.globalState.update(KEY_LAST_KNOWN_VERSION, currentVersion);
-            }
-            catch (e) {
-                me.log(`[ERROR] Controller.showNewVersionPopup(1): ${cj_helpers.toStringSafe(e)}`);
-            }
+                await this._CONTEXT.globalState.update(
+                    KEY_SHOW_DEPRECATED_MESSAGE,
+                    newShowMessageValue
+                );
+            } catch { }
         }
     }
 
